8bced1fb6c0865bfbd5a0c76196666bfd23baaab
Ben Ripkens <bripkens.dev@gmail.com>
2011-09-17 08:37:27 +0200

    Bare rails 3.1.0 project created.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opr::Application.config.secret_token = '332c69ee157b726edc7ed8f9a2f09ea3891d8ee2750d6ed3411dc4005e9c49cb43c7773ed13a32152c6a57f93746d9241250d998c969e66241bb595c1b3b2e76'
