228ee568db44d7be1500daede035568c773764e9
Ben Ripkens <bripkens.dev@gmail.com>
2011-09-17 09:37:25 +0200

    Working rails 3.1.0 project structure.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Opr::Application.config.secret_token = '332c69ee157b726edc7ed8f9a2f09ea3891d8ee2750d6ed3411dc4005e9c49cb43c7773ed13a32152c6a57f93746d9241250d998c969e66241bb595c1b3b2e76'
+Opr::Application.config.secret_token = 'cadbebf2f72a25a9599aef36b2cb2dae071bfed815ad83f085f5ba4b620bcf106b9d49640c2be0aa96c5ec8ecd186f4d80d4c3d8cd16fa297205a0216e9165a5'
